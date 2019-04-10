3b9ccb2d4704a3d273ac7a3a54d3d937ffa2f77f
mu88 <mu88_developer@outlook.com>
2019-04-10 19:22:39 +0200

    Introduce projects for automated tests and business logic

---
+++ Tests/SampleTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class SampleTest
+    {
+        [Test]
+        public void TheTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
